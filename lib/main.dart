8a77050f8d196d4d97f1bca8de6080e819015c6c
ECLKnight <pepi91cl@gmail.com>
2019-11-03 15:47:49 -0500

    message on main

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,8 @@
 /// easier.
 /// Emanuel
 
-import 'package:flutter/material.dart';
 import 'package:flash_chat/screens/welcome_screen.dart';
-import 'package:flash_chat/screens/login_screen.dart';
-import 'package:flash_chat/screens/registration_screen.dart';
-import 'package:flash_chat/screens/chat_screen.dart';
+import 'package:flutter/material.dart';
 
 void main() => runApp(FlashChat());
 
@@ -18,7 +15,6 @@ class FlashChat extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         textTheme: TextTheme(
           body1: TextStyle(color: Colors.black54),
-
         ),
       ),
       home: WelcomeScreen(),
