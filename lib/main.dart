5c90fabe788bb540d7e7b882c403b56c0144a503
ECLKnight <pepi91cl@gmail.com>
2019-11-03 20:58:24 -0500

    learn about more about routes static key word

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,12 @@
 /// easier.
 /// Emanuel
 /// DAY 1
-
-import 'package:flash_chat/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
+
+import 'screens/chat_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/registration_screen.dart';
+import 'screens/welcome_screen.dart';
 
 void main() => runApp(FlashChat());
 
@@ -13,12 +16,19 @@ class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         textTheme: TextTheme(
           body1: TextStyle(color: Colors.black54),
         ),
       ),
-      home: WelcomeScreen(),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen()
+      },
     );
   }
 }
