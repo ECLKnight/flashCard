2c466736b7a82b6ed0f47597069cd9bafa4fa536
ECLKnight <pepi91cl@gmail.com>
2019-11-05 02:46:35 -0500

    Refactor the code to use RoundedButton

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -2,6 +2,7 @@ import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flash_chat/components/round_button.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static const String id = 'welcome_screen';
@@ -111,35 +112,6 @@ class _WelcomeScreenState extends State<WelcomeScreen>
               },
             ),
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class RoundedButton extends StatelessWidget {
-  final Color roundButtonColor;
-  final String roundButtonLabel;
-  final Function onPressRoute;
-
-  RoundedButton(
-      {this.roundButtonColor, this.roundButtonLabel, this.onPressRoute});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(vertical: 16.0),
-      child: Material(
-        elevation: 5.0,
-        color: roundButtonColor,
-        borderRadius: BorderRadius.circular(30.0),
-        child: MaterialButton(
-          onPressed: onPressRoute,
-          minWidth: 200.0,
-          height: 42.0,
-          child: Text(
-            roundButtonLabel,
-          ),
         ),
       ),
     );
